4c3e07401646ec2ca2cedaac5bd5adcd718cbd57
krishpansara <krishpanasara9265@gmail.com>
2025-07-31 18:08:51 +0530

    login + splash screen

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:first/notes_app/notes.dart';
+// import 'package:first/notes_app/notes.dart';
+// import 'package:first/e-com/login_page.dart';
+import 'package:first/e-com/splash_screen.dart';
 import 'package:flutter/material.dart';
 // import 'e-com/cart_page.dart';
 
@@ -20,7 +22,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       // home: CartPage(),
-      home: NotesScreen(),
+      // home: NotesScreen(),
+      // home: LoginPage(),
+      home: SplashScreen(),
 
     );
   }
